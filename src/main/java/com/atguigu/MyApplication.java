5a6208147653ffbc325f41c3d7f3a44979cc09ad
zfj <2505019207@qq.com>
2020-09-05 09:15:46 +0800

    增加11111MMMM

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -18,7 +18,7 @@ public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
 
-        System.out.println("11111");
+        System.out.println("11111MMMM");
 
     }
 }
